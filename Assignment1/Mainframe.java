6cf121185d9607348a97aa8603216ad928761295
devern-DZZC <devern.chattergoon@my.uwi.edu>
2025-02-01 19:08:57 -0400

    created Mainframe class

--- Assignment1/Mainframe.java
+++ Assignment1/Mainframe.java
@@ -1,11 +1,13 @@
 import java.util.ArrayList;
+import java.time.LocalDate;
+import java.time.Period;
 
 public class Mainframe{
     private ArrayList<InsuredPerson> clients;
-    private int MAX_DATE;
+    private final int MAX_DATE;
     
     public Mainframe(){
-        this.clients = new ArrayList<InsuredPerson>(100);
+        this.clients = new ArrayList<InsuredPerson>();
         this.MAX_DATE = 60;
     }
     
@@ -14,5 +16,47 @@ public class Mainframe{
         this.clients.add(person);
     }
     
-    public void addClaim
+    public void addClaim(int clientIndex, String claimDate, double claimValue){
+        if(clientIndex > 0 && clientIndex < clients.size()){
+            InsuredPerson person = this.clients.get(clientIndex);
+            if(person != null)
+                person.addClaim(claimDate, claimValue);
+        }else
+            System.out.println("Error...");
+    }
+    
+    public boolean validateDayRange(LocalDate claimDate){
+        LocalDate currDate = LocalDate.now();
+        Period period = Period.between(claimDate, currDate);
+        int days = period.getDays();
+        if(days > 60 || claimDate.isAfter(currDate))
+            return false;
+        else
+            return true;
+    }
+    
+    public void processClaims(){
+        for(InsuredPerson client: clients){
+            ArrayList<Claim> claims = client.getClaims();
+            for(Claim claim: claims){
+                if(this.validateDayRange(claim.getClaimDate())){
+                    claim.disburse(true);
+                    client.recordDisbursements(claim.getClaimValue());
+                }
+                else{
+                    claim.disburse(false);
+                    claim.setClaimNote("CLAIM OLDER THAN 60 DAYS / INVALID DATE");
+                }
+            }
+        }
+    }
+    
+    public String getReports(){
+        String output="";
+        for(InsuredPerson client: clients){
+            if(client != null)
+                output += client.toString() + "\n";
+        }
+        return output;
+    }
 }
