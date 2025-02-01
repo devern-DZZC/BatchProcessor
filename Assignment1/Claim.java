444c4c822e9d5579f5c010087c05af639872f626
devern-DZZC <devern.chattergoon@my.uwi.edu>
2025-02-01 13:42:31 -0400

    Claim class methods

--- Assignment1/Claim.java
+++ Assignment1/Claim.java
@@ -1,19 +1,60 @@
 import java.time.LocalDate;
+import java.text.NumberFormat;
 
 public class Claim{
-    String claimNo;
-    int claimCounter;
-    LocalDate claimDate;
-    LocalDate processedDate;
-    double claimValue;
-    boolean disbursed;
-    String claimNote;
+    private String claimNo;
+    private int claimCounter = 0;
+    private LocalDate claimDate;
+    private LocalDate processedDate;
+    private double claimValue;
+    private boolean disbursed;
+    private String claimNote;
     
     public Claim(String claimDate, double claimValue, String claimOwnerInitials){
         this.claimDate = LocalDate.parse(claimDate);
         this.claimValue = claimValue;
-        this.claimNo = claimOwnerInitials;
+        this.claimCounter++;
+        this.claimNo = claimOwnerInitials + claimCounter;
         
     }
+    
+    public String getClaimNo(){return this.claimNo; }
+    public LocalDate getClaimDate(){return this.claimDate;}
+    public double getClaimValue() {return this.claimValue;}
+    
+    public void disburse(boolean disbursed){
+        this.disbursed = disbursed;
+        this.processedDate = LocalDate.now();
+    }
+    
+    public void setClaimNote(String note){
+        if(note != null){
+            this.claimNote = "\t" + note;
+        }
+        return;
+    }
+    
+    public String toString(){
+        String bool = "";
+        if (this.disbursed){
+            bool = "Y";
+        }
+        else{
+            bool = "N";
+        }
+        
+        NumberFormat currency = NumberFormat.getCurrencyInstance();
+        String formattedAmount = currency.format(getClaimValue());
+        
+        String output ="CLAIM NO: " + getClaimNo() + " " + getClaimDate() + " " + formattedAmount + " PAID: " + bool + 
+        " PROCESSED ON: " + this.processedDate;
+        
+        if (this.claimNote != null){
+            output += "\n NOTE:" + this.claimNote;
+        }
+        
+        return output;
+    }
+    
     
 }
